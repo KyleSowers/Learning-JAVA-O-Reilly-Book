2adf8d02c812b48a94cbc49f322832af621a6bf7
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-31 08:55:41 -0500

    Added JLabel to display text on page.

--- HelloJava.java
+++ HelloJava.java
@@ -4,6 +4,8 @@ public class HelloJava {
     public static void main(String[] args) {
 //        System.out.println("Hello, Java!");
         JFrame frame = new JFrame("Hello, Java!");
+        JLabel label = new JLabel("Hello, Java!", JLabel.CENTER );
+        frame.add(label);
         frame.setSize(300, 300 );
         frame.setVisible(true);
     }
