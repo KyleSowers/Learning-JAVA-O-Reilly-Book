2ee05b4829312547c4988eeb8b69d68bd80f4e50
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-26 12:51:12 -0500

    Using the add() method, to add a new instance of a HelloComponent2 method that displays a message, to the JFram "frame".

--- src/HelloJava2.java
+++ src/HelloJava2.java
@@ -7,6 +7,6 @@ public class HelloJava2 {
     public static void main(String[] args) {
 
         JFrame frame = new JFrame("HelloJava2");
-
+        frame.add( new HelloComponent2("Hello, Java!"));
     }
 }
