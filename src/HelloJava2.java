eb2cdcb9c7f9065cd2b853767ba0db52a9cc0b0b
Kyle Sowers <kyle.a.sowers@gmail.com>
2024-01-26 12:58:52 -0500

    The previous comment can extend here to say I learned that without the setDefaultCloseOperation() method, the program mearly closes and will likely still run until program is manually stopped, versus the addition of this command telling the window to close and the program to stop running.Then in this comment, added the setSize() method for "frame" to a width and height of 300 for the window display.

--- src/HelloJava2.java
+++ src/HelloJava2.java
@@ -9,5 +9,6 @@ public class HelloJava2 {
         JFrame frame = new JFrame("HelloJava2");
         frame.add( new HelloComponent2("Hello, Java!"));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize( 300, 300);
     }
 }
